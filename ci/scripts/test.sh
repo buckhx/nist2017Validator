7bf08990bbaa77335f8841db363b283fa591dab1
Adam Minasian <minasian.adam@gmail.com>
2018-02-05 10:48:10 -0500

    updated test.sh

--- ci/scripts/test.sh
+++ ci/scripts/test.sh
@@ -9,10 +9,12 @@ apk add --no-cache git mercurial curl \
  && curl -L -s https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64 -o $GOPATH/bin/dep \
  && chmod +x $GOPATH/bin/dep
 
+ls $GOPATH/bin/dep
+
 mkdir -p src/github.com/aminasian/
 cp -R ./nist-source src/github.com/aminasian/.
 
-dep ensure
+$GOPATH/bin/dep ensure
 
 go test -cover ./... | tee test_coverage_results.txt
 sed -i -e 's/^/     /' test_coverage_results.txt
