f8ba07de0d493ff2e44505fd750b0e92f647bcdc
Adam Minasian <minasian.adam@gmail.com>
2018-02-05 09:58:26 -0500

    updated test script to install go dep

--- ci/scripts/test.sh
+++ ci/scripts/test.sh
@@ -6,7 +6,8 @@ export GOPATH=$PWD
 
 mkdir -p src/github.com/aminasian/
 cp -R ./nist-source src/github.com/aminasian/.
-
+go get -u github.com/golang/dep/cmd/dep
+dep ensure
 go test -cover ./... | tee test_coverage_results.txt
 sed -i -e 's/^/     /' test_coverage_results.txt
 
